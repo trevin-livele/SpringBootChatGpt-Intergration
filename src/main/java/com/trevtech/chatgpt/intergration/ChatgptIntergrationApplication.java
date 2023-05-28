f1710188e3077d3a86f13a98ab665f63c80e57bf
trevin-livele <liveletrevin6@gmail.com>
2023-05-28 10:49:50 +0300

    added intergration code

---
+++ src/main/java/com/trevtech/chatgpt/intergration/ChatgptIntergrationApplication.java
@@ -0,0 +1,13 @@
+package com.trevtech.chatgpt.intergration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptIntergrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptIntergrationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/trevtech/chatgpt/intergration/dto/ChatGptResponse.java
@@ -0,0 +1,22 @@
+package com.trevtech.chatgpt.intergration.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatGptResponse {
+
+    private List<Choice> choices;
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Choice{
+        private int index;
+        private Message message;
+    }
+}
